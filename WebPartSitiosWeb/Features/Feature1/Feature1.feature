129a8d0d50e651605d94520012952517eaf2d574
learrsan <gaslas@hotmail.com>
2015-03-27 16:33:36 +0000

    24/03/15

---
+++ WebPartSitiosWeb/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="0d108cb7-1afa-4940-b8b7-0ef2c244a817" featureId="0d108cb7-1afa-4940-b8b7-0ef2c244a817" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="WebPartSitiosWeb Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="8351c4af-e262-44eb-ac20-1bf03d8a9f28" />
+  </projectItems>
+</feature>
---
+++ WebPartSitiosWeb/VisualWebPart1/VisualWebPart1UserControl.ascx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Label = System.Reflection.Emit.Label;
+
+namespace WebPartSitiosWeb.VisualWebPart1
+{
+    public partial class VisualWebPart1UserControl : WebPart
+    {
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var sitio = SPContext.Current.Site;
+            Label1.Text = sitio.Url;
+            lstSitios.Items.Clear();
+
+            foreach (SPWeb web in sitio.AllWebs)
+            {
+                lstSitios.Items.Add(new ListItem(web.Title,web.ID.ToString()));
+            }
+        }
+        
+        }
+    }
+
